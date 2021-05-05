1db0aa268ede18fe668dad78d26ee2821bf313d0
Pedro Henrique de Oliveira <pedro.o@softplan.com.br>
2021-05-05 15:15:36 -0300

    ok

---
+++ src/config/pichau.ts
@@ -0,0 +1,9 @@
+export default 
+["https://www.pichau.com.br/hardware/placa-de-video?rgpu=767%2C700",
+"https://www.pichau.com.br/hardware/placa-de-video?rgpu=748",
+"https://www.pichau.com.br/hardware/placa-de-video?rgpu=550",
+"https://www.pichau.com.br/hardware/placa-de-video?rgpu=747%2C671",
+"https://www.pichau.com.br/hardware/placa-de-video?rgpu=6359%2C6343",
+"https://www.pichau.com.br/hardware/placa-de-video?rgpu=611%2C686%2C844%2C741%2C742",
+"https://www.pichau.com.br/hardware/placa-de-video?rgpu=6304%2C6305"
+]
---
+++ src/config/terabyte.ts
@@ -0,0 +1,3 @@
+export default 
+["https://www.terabyteshop.com.br/busca?str=placa+de+v%C3%ADdeo+gtx+1660+super"
+]
