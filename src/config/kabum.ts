9748ae7f00d315476528d97d6b86a932f6e94fc1
Pedro <pedrooliveira__@hotmail.com>
2021-05-10 21:00:34 -0300

    add gpu link kabum

--- src/config/kabum.ts
+++ src/config/kabum.ts
@@ -5,5 +5,9 @@ export default
 "https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rtx+3060&btnG=",
 "https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rtx+3080&btnG=",
 "https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rx+6700&btnG=",
-"https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rtx+2060&btnG="
+"https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rtx+2060&btnG=",
+"https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rx+570&btnG=",
+"https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rx+5700&btnG=",
+"https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rx+6800&btnG=",
+"https://www.kabum.com.br/cgi-local/site/listagem/listagem.cgi?string=video+rx+580&btnG="
 ]
